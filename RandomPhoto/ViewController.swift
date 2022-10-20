49aca1dd25333d4f5f935465a91b91b10531e923
Leehan Choi <devprworld@gmail.com>
2022-10-20 17:39:35 +0900

    system background color changed

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue // 시스템 백그라운드 컬러 설정
     }
 
 
